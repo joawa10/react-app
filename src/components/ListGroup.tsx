f7304c3df25531b534b6d0740747c96406873541
joawa10 <joawa10@yahoo.com>
2024-04-02 16:04:01 +0800

    create ListGroup Component

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+}
+
+function ListGroup({ items, heading }: Props) {
+  //selectedIndex = state variable holding the current state value
+  //setSelectedIndex = function that updates the state
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
+  return (
+    <>
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
