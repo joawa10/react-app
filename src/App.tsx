f00c17819a220f3858e755218355fb28a6ad7067
joawa10 <joawa10@yahoo.com>
2024-04-03 18:26:30 +0800

    created form component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandableText maxChars={150}>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
-        consectetur consequatur aut aperiam ab accusamus cum nisi accusantium
-        reprehenderit ex quo reiciendis debitis, vero aliquid iste numquam.
-        Eaque, labore facilis eligendi ea enim, perspiciatis quaerat voluptatum
-        inventore fuga architecto laboriosam quis. Optio sequi nisi aliquid quos
-        quasi quae debitis soluta odit, possimus inventore officiis iure
-        accusamus cum consequatur, voluptatum hic maiores neque eos, ea mollitia
-        labore quis est nihil! Inventore nulla consequuntur consectetur rerum
-        voluptate nisi optio numquam, obcaecati porro molestias sint impedit
-        nostrum non iusto repudiandae aspernatur ducimus accusamus nemo culpa.
-        Corrupti, repellendus. Sit doloremque eaque cupiditate eveniet
-        voluptate.
-      </ExpandableText>
+      <Form></Form>
     </div>
   );
 }
