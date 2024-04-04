ea8c28ffbdc3902241709ace8e7198fa2e4bb289
joawa10 <joawa10@yahoo.com>
2024-04-04 13:53:10 +0800

    added delete function using state hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,19 @@
+import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  const expenses = [
-    {
-      id: 1,
-      description: "aaa",
-      amount: 10,
-      category: "Utilities",
-    },
-    {
-      id: 2,
-      description: "bbb",
-      amount: 11,
-      category: "Utilities",
-    },
-    {
-      id: 3,
-      description: "ccc",
-      amount: 12,
-      category: "Utilities",
-    },
-    {
-      id: 4,
-      description: "ddd",
-      amount: 13,
-      category: "Utilities",
-    },
-  ];
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 11, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 12, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 13, category: "Utilities" },
+  ]);
 
   return (
     <div>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log("delete", id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   );
